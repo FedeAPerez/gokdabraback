db4c94d4338534133252f3413f20ff1169da013a
fedea <fedeaperez@hotmail.com.ar>
2018-04-30 18:24:54 -0300

    log de req

--- app/routes/watson_routes.js
+++ app/routes/watson_routes.js
@@ -3,6 +3,7 @@
 module.exports = function(app) {
 	app.post('/intent',
 		(req, res) => {
+			console.log(req);
 	    	// Gets the intent from watson
 	    	var AssistantV1 = require('watson-developer-cloud/assistant/v1');
 	    	var watsonAssistant = new AssistantV1({
