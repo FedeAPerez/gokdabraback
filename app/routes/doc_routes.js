016c795ef99ebb25c29c60d44a77dcc8c9b4b09f
fedea <fedeaperez@hotmail.com.ar>
2018-04-30 17:14:40 -0300

    compile with cors

--- app/routes/doc_routes.js
+++ app/routes/doc_routes.js
@@ -1,6 +1,11 @@
 // routes/doc_routes.js
 
 module.exports = function(app) {
+	app.all('/*', function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	  next();
+	});
 	app.get('/doc',
 		(req, res) => {
 		    console.log("PING PING PING");
