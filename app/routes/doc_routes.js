fcb3e0644939aa6c49341a9fce1e83ff4bfb7f5a
fedea <fedeaperez@hotmail.com.ar>
2018-04-30 17:19:41 -0300

    segunda prueba de request

--- app/routes/doc_routes.js
+++ app/routes/doc_routes.js
@@ -1,11 +1,7 @@
 // routes/doc_routes.js
 
 module.exports = function(app) {
-	app.all('/*', function(req, res, next) {
-	  res.header("Access-Control-Allow-Origin", "*");
-	  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	  next();
-	});
+
 	app.get('/doc',
 		(req, res) => {
 		    console.log("PING PING PING");
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const port 				= 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 require('./app/routes')(app);
 
 app.listen((process.env.PORT || 5000), () => {
