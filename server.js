7069c23ea14051018dafc84648556e29d2eb25bd
fedea <fedeaperez@hotmail.com.ar>
2018-04-30 17:23:14 -0300

    origin from cors library

--- server.js
+++ server.js
@@ -3,15 +3,12 @@ const express        	= require('express');
 const bodyParser     	= require('body-parser');
 const app            	= express();
 
+var cors = require('cors')
 const port 				= 8000;
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 require('./app/routes')(app);
 
