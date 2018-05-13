923cdab4d18e3c619ff8e61b2d623e2561d83435
fedea <fedeaperez@hotmail.com.ar>
2018-05-13 17:50:58 -0300

    business

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 // routes/index.js
 const watsonRoutes = require('./watson_routes');
 const docRoutes = require('./doc_routes');
-const businessRoutes = require('./businessRoutes');
+const businessRoutes = require('./business_routes');
 
 module.exports = function(app) {
   watsonRoutes(app);
