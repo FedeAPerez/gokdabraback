edba8e9e2147489a575d70ebf4746a849a52ba4f
fedea <fedeaperez@hotmail.com.ar>
2018-05-13 20:41:28 -0300

    basic content

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,11 @@
 const watsonRoutes = require('./watson_routes');
 const docRoutes = require('./doc_routes');
 const businessRoutes = require('./business_routes');
+const messagesContentRoutes = require('./messages_content_routes');
 
 module.exports = function(app, db_client) {
   watsonRoutes(app, db_client);
   docRoutes(app, db_client);
   businessRoutes(app, db_client);
+  messagesContentRoutes(app, db_client);
 };
---
+++ app/routes/messages_content_routes.js
@@ -0,0 +1,40 @@
+// routes/messages_content_routes.js
+const env = process.env.DV_ENV;
+
+module.exports = function(app, db_client) {
+	app.get('/contentmessages', 
+		(req,res) => {
+			const prospectsCollection = db_client.db(env).collection("contentmessages");
+			prospectsCollection.find({}).toArray()
+			.then((getResult) => {
+				console.log(getResult);
+				res.status(200).send({
+					"route":"contentmessages",
+					"operation":"GET",
+					"data": getResult
+				});
+			})
+			.catch((err) => {
+
+			});
+
+		}
+	);
+
+	app.post('/contentmessages',
+		(req, res) => {
+			const prospectsCollection = db_client.db(env).collection("contentmessages");
+			prospectsCollection.insertOne({
+				id_content_message : req.body.id_content_message,
+				content_message : req.body.content_message
+			}, function(err, res) {
+				console.log("errores " + err);
+				console.log("resultado " + res);
+			});
+			res.status(204).send({
+				"route":"contentmessages",
+				"operation":"POST"
+			});
+  		}
+  	);
+};
