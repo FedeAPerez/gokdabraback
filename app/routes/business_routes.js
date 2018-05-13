efbb0105f980c253ddee7afc039e96bd5b4a96c7
fedea <fedeaperez@hotmail.com.ar>
2018-05-13 13:56:14 -0300

    back con business

---
+++ app/routes/business_routes.js
@@ -0,0 +1,9 @@
+// routes/business_routes.js
+
+module.exports = function(app) {
+	app.post('/business',
+		(req, res) => {
+			res.status(200);
+  		}
+  	);
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,10 @@
 // routes/index.js
 const watsonRoutes = require('./watson_routes');
 const docRoutes = require('./doc_routes');
+const businessRoutes = require('./businessRoutes');
+
 module.exports = function(app) {
   watsonRoutes(app);
   docRoutes(app);
+  businessRoutes(app);
 };
