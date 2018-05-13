257f214cd23537785a1609b8dfdb6625514be270
fedea <fedeaperez@hotmail.com.ar>
2018-05-13 18:30:34 -0300

    guardo mails

--- app/routes/business_routes.js
+++ app/routes/business_routes.js
@@ -1,6 +1,17 @@
 // routes/business_routes.js
 
 module.exports = function(app, db_client) {
+	app.get('/business', 
+		(req,res) => {
+			const prospectsCollection = db_client.db("test").collection("prospects");
+			var getResult = prospectsCollection.find().toArray();
+			res.status(200).send({
+				"route":"business",
+				"operation":"GET",
+				"data": getResult
+			});
+		}
+	);
 	app.post('/business',
 		(req, res) => {
 			const prospectsCollection = db_client.db("test").collection("prospects");
@@ -10,7 +21,10 @@ module.exports = function(app, db_client) {
 				console.log("errores " + err);
 				console.log("resultado " + res);
 			});
-			res.status(200).send({});
+			res.status(204).send({
+				"route":"business",
+				"operation":"POST"
+			});
   		}
   	);
 };
